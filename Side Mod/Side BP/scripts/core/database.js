ea64599c57be9097ba032bebdd0028d2a77210b4
darkness9373 <ogustiantpharell@gmail.com>
2026-03-09 19:35:08 +0700

    update database.js

--- Side Mod/Side BP/scripts/core/database.js
+++ Side Mod/Side BP/scripts/core/database.js
@@ -17,3 +17,30 @@ export function setData(key, value) {
 export function deleteData(key) {
     world.setDynamicProperty(key, undefined);
 }
+
+export function getFishcoin(playerName) {
+    const db = world.getDynamicProperty(`fishcoin:${playerName}`);
+    return db ?? 0;
+}
+
+export function setFishcoin(playerName, value) {
+    world.setDynamicProperty(`fishcoin:${playerName}`, value)
+}
+
+export function getDungeoncoin(playerName) {
+    const db = world.getDynamicProperty(`dungeoncoin:${playerName}`);
+    return db ?? 0;
+}
+
+export function setDungeoncoin(playerName, value) {
+    world.setDynamicProperty(`dungeoncoin:${playerName}`, value)
+}
+
+export function getGold(playerName) {
+    const db = world.getDynamicProperty(`gold:${playerName}`);
+    return db ?? 0;
+}
+
+export function setGold(playerName, value) {
+    world.setDynamicProperty(`gold:${playerName}`, value)
+}
