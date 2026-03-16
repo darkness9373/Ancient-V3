a5ff41c6b7fcbf147278d7b22282de68dcf064eb
Darkness <darkness.9373@gmail.com>
2026-03-16 13:15:04 +0700

    p

--- Side Mod/Side BP/scripts/commands/mkevent.js
+++ Side Mod/Side BP/scripts/commands/mkevent.js
@@ -3,38 +3,37 @@ import { ModalFormData } from "@minecraft/server-ui";
 
 let event = null;
 
-system.beforeEvents.startup.subscribe(data => {
+system.beforeEvents.startup.subscribe((data) => {
     data.customCommandRegistry.registerCommand({
-        name: 'as:mkevent',
+        name: "as:mkevent",
         description: "Make an event",
         cheatsRequired: false,
         permissionLevel: 1
     }, (origin) => {
         const player = origin.sourceEntity;
-        if (!(player instanceof Player)) return;
-        system.run(() => makeEventForm(player))
+        if (!(player instanceof Player)) return
+        if (event) return player.sendMessage('§c[!] An event is already running');
+        system.run(() => openEventForm(player))
     })
 })
 
 /** @param {Player} player */
-function makeEventForm(player) {
+function openEventForm(player) {
     const form = new ModalFormData()
     form.title('Make Event')
-    form.textField('Event Name', 'Dungeon raid')
-    form.textField('Countdown (in seconds)', '300')
+    form.textField('Event Name', 'Dungeon Raid')
+    form.textField('Countdown (in seconds)', '300', { defaultValue: '300' })
     form.toggle('Teleport when countdown ends', { defaultValue: true })
-    form.textField('Teleport Coordinates', '1000 1000 1000')
-    form.textField('Tag added to boss bar', 'DungeonRaid')
-    form.textField('Dimension', 'overworld, nether, the_end')
-    form.show(player).then(r => {
-        if (r.canceled) return;
-        const eventName = r.formValues[0].trim();
-        if (!eventName || eventName.length < 3) return player.sendMessage('§c[!] Event name must be at least 3 characters');
-        const countdown = Number(r.formValues[1].trim());
-        if (isNaN(countdown) || countdown < 0) return player.sendMessage('§c[!] Countdown must be a positive number');
-        const teleport = r.formValues[2];
-        let coords = r.formValues[3].trim();
-        const tag = r.formValues[4].trim();
+    form.textField('Teleport Coordinates', '1000 1000 1000', { defaultValue: '1000 1000 1000' })
+    form.textField('Dimension', 'overworld, nether, the_end', { defaultValue: 'overworld' })
+    form.show(player).then((r) => {
+        if (r.canceled) return
+        const eventName = r.formValues[0].trim()
+        if (!eventName || eventName.length < 3) return player.sendMessage('§c[!] Event name must be at least 3 characters')
+        const countdown = Number(r.formValues[1].trim())
+        if (isNaN(countdown) || countdown < 0) return player.sendMessage('§c[!] Countdown must be a positive number')
+        const teleport = r.formValues[2]
+        let coords = r.formValues[3].trim()
         if (!teleport) coords = null;
         let loc = null;
         if (coords) {
@@ -42,73 +41,39 @@ function makeEventForm(player) {
             if (loc.length !== 3) return player.sendMessage('§c[!] Teleport coordinates must be in the format of x y z');
             if (isNaN(Number(loc[0])) || isNaN(Number(loc[1])) || isNaN(Number(loc[2]))) return player.sendMessage('§c[!] Teleport coordinates must be a number');
         }
-        coords = loc ? `${loc[0]} ${loc[1]} ${loc[2]}` : null;
-        spawnEvent(eventName, countdown, coords, tag, r.formValues[5].trim());
+        coords = loc ? {
+            x: Number(loc[0]),
+            y: Number(loc[1]),
+            z: Number(loc[2])
+        } : null;
+        const dimension = r.formValues[5].trim()
+        runEvent(eventName, countdown, coords, teleport, dimension);
     })
 }
 
-function spawnEvent(eventName, countdown, coords, tag, dimension) {
-    const dim = [
-        world.getDimension('overworld'),
-        world.getDimension('nether'),
-        world.getDimension('the_end')
-    ]
-    for (const d of dim) {
-        const entity = d.spawnEntity('as:event', {
-            x: 0,
-            y: -64,
-            z: 0
-        })
-        entity.addTag(tag)
-        entity.setDynamicProperty(tag, countdown)
-        entity.setDynamicProperty('name', eventName)
-        entity.setDynamicProperty('coords', coords)
-    }
-    runEvent(eventName, countdown, coords, tag, dimension);
-}
-
-function runEvent(eventName, countdown, coords, tag, dimension) {
+function runEvent(eventName, countdown, coords, teleport, dimension) {
+    if (event) return player.sendMessage('§c[!] An event is already running');
+    let cd = countdown;
     event = system.runInterval(() => {
-        const dim = [
-            world.getDimension('overworld'),
-            world.getDimension('nether'),
-            world.getDimension('the_end')
-        ]
-        for (const d of dim) {
-            const entities = d.getEntities({
-                type: 'as:event',
-                tags: [tag]
-            })[0]
-            const cdMax = countdown;
-            const cd = entities.getDynamicProperty(tag);
-            entities.setDynamicProperty(tag, cd - 1)
-            const health = (cd / cdMax) * 100;
-            const hpCom = entities.getComponent('minecraft:health');
-            hpCom.setCurrentValue(health);
-
-            const min = Math.floor(cd / 60);
-            let sec = cd % 60;
-            entities.nameTag = `${eventName} (${min}:${sec.toString().padStart(2, '0')})`;
-            if (cd <= 0) {
-                system.clearRun(event);
-                event = null;
-                entities.remove();
-                const loc = coords.split(' ');
-                if (loc) {
-                    for (const p of world.getPlayers()) {
-                        p.teleport({
-                            x: Number(loc[0]),
-                            y: Number(loc[1]),
-                            z: Number(loc[2])
-                        }, {
-                            dimension: world.getDimension(dimension)
-                        });
-                        p.sendMessage(`§a[!] The ${eventName} event has started`);
-                    }
-                } else {
-                    for (const p of world.getPlayers()) {
-                        p.sendMessage(`§a[!] The ${eventName} event has started`);
-                    }
+        const min = Math.floor(cd / 60);
+        const sec = cd % 60;
+        const time = `${min}:${sec.toString().padStart(2, '0')}`;
+        for (const p of world.getPlayers()) {
+            p.onScreenDisplay.setActionBar(`§a${eventName} §estarts in ${time}`);
+        }
+        if (cd <= 0) {
+            system.clearRun(event);
+            event = null;
+            for (const p of world.getPlayers()) {
+                p.sendMessage(`§a[!] The ${eventName} event has started`);
+                if (teleport && coords) {
+                    p.teleport({
+                        x: coords.x,
+                        y: coords.y,
+                        z: coords.z
+                    }, {
+                        dimension: world.getDimension(dimension)
+                    });
                 }
             }
         }
